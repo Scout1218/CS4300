6014fe012319c8ea1461412b06b1907b66a4af11
Grant Wehrli <grantwehrli18@gmail.com>
2025-09-17 22:34:15 +0000

    I lied actually I am rewirting all the test cases to only do one at a time

--- homework1/tests/task2_test.py
+++ homework1/tests/task2_test.py
@@ -22,8 +22,14 @@ string = "hello"
 
 
 #########################TESTS###############################
-def test_datatype():
+def test_add_ints():
     assert isinstance(add_integers(x,y), int)
+
+def test_divide_ints():
     assert isinstance(divide_integers(x,y), float)
+
+def test_string():
     assert isinstance(uppercase(string), str)
+
+def test_is_even():
     assert isinstance(is_even(x), bool)
