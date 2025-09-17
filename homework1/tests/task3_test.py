ce3d46b30b3b6d8f50c14e194458a66d436fc2fd
Grant Wehrli <grantwehrli18@gmail.com>
2025-09-17 22:35:10 +0000

    reformatted test2

--- homework1/tests/task3_test.py
+++ homework1/tests/task3_test.py
@@ -27,10 +27,18 @@ def sum_1_to_n(n):
 
 
 #########################TESTS###############################
-def test_results():
+def test_pos_neg_zero_positive():
     assert pos_neg_zero(3) == "positive"
+
+def test_pos_neg_zero_negative():
     assert pos_neg_zero(-3) == "negative"
+
+def test_pos_neg_zero_zero():
     assert pos_neg_zero(0) == "zero"
+
+def test_n_primes():
     assert n_primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+def test_sum_1_to_n():
     assert sum_1_to_n(100) == 5050
     
