5603a1039a22f2773fff82650fe95a5c8cd9fb90
Grant Wehrli <grantwehrli18@gmail.com>
2025-09-19 17:20:36 +0000

    task7test

---
+++ homework1/tests/task7_test.py
@@ -0,0 +1,9 @@
+from task7 import summarize
+
+###################################TESTS###########
+
+def test_summarize()
+    stats = summarize([1,2,3,4,5])
+    assert stats["mean"] == 3
+    assert stats["median"] == 3
+    assert np.isclose(stats["std"], np.std([1, 2, 3, 4, 5], ddof=0))
