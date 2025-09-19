046c429312450fc5354a1888e699c5b6a4c0ee44
Grant Wehrli <grantwehrli18@gmail.com>
2025-09-19 17:23:45 +0000

    small fix to test

--- homework1/tests/task7_test.py
+++ homework1/tests/task7_test.py
@@ -1,8 +1,8 @@
 from task7 import summarize
-
+import numpy as np
 ###################################TESTS###########
 
-def test_summarize()
+def test_summarize():
     stats = summarize([1,2,3,4,5])
     assert stats["mean"] == 3
     assert stats["median"] == 3
